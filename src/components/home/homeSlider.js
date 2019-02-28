085216d14f8aa9eea07d7fe3da2b4bedb4a6cb99
italo fontes <italo5123@hotmail.com>
2019-02-28 15:28:12 -0300

    slider fixed

--- src/components/home/homeSlider.js
+++ src/components/home/homeSlider.js
@@ -1,56 +1,92 @@
 import React, { Component } from 'react';
 import { Link }  from 'gatsby';
+import slides from '../../helpers/slides';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-const slides = [
-    {
-        slideTitle: 'Te ajudamos a ingressar no mundo digital!',
-        slideSubtitle: 'Desenvolvemos produtos inovadorese modernos que trazem resultados',
-    },
-    {
-        slideTitle: 'batata',
-        slideSubtitle: 'Desenvolvemos produtos inovadorese modernos que trazem resultados',
-    },
-    {
-        slideTitle: 'cenoura',
-        slideSubtitle: 'Desenvolvemos produtos inovadorese modernos que trazem resultados',
-    }
-];
 
 export default class Slider extends Component{
+
+    intervalId = 0;
+
     constructor(){
         super();
-
         this.state = {
             currentSlide: slides[0],
             slideCount: 0
         }
         this.changeSlide = this.changeSlide.bind(this);
+        this.buttonChange = this.buttonChange.bind(this);
+        this.intervalHandler = this.intervalHandler.bind(this);
     }
 
     componentDidMount(){
-        window.setInterval(this.changeSlide, 8000);
+        this.intervalHandler("start");
     }
 
     componentWillUnmount(){
-        window.clearInterval(this.changeSlide, 8000);
+        this.intervalHandler('remove');
     }
 
     changeSlide(){
-        if(this.state.slideCount === slides.length - 1){
+
+        if(this.state.slideCount === slides.length - 1) {
+
             this.setState({currentSlide: slides[0], slideCount: 0});
+
         }else{
+
             let newIndex = this.state.slideCount + 1;
             this.setState({currentSlide : slides[newIndex], slideCount: newIndex});
+
+        }
+    }
+
+    buttonChange(e){
+        this.intervalHandler('remove');
+        const changeTo = parseInt(e.target.dataset.slide);
+        this.setState({currentSlide: slides[changeTo], slideCount: changeTo});
+        this.intervalHandler('start');
+
+    }
+
+    intervalHandler(action){
+        if(action === "start"){
+
+            this.intervalId = window.setInterval(this.changeSlide, 8000);
+
+        }else if(action === "remove"){
+          
+            window.clearInterval(this.intervalId);
+
         }
     }
 
     render(){
         return(
             <div className="homeSlider">
-                <div className="homeSlider__container">
-                    <h1 className="homeSlider__title">{this.state.currentSlide.slideTitle}</h1>
-                    <h3 className="homeSlider__desc">{this.state.currentSlide.slideSubtitle}</h3>
-                    <Link to="/about" className="btn">Mais info</Link>
+                <TransitionGroup className="homeSlider__container">
+                    <CSSTransition
+                        timeout={4500}
+                        classNames="slide"
+                        key={this.state.currentSlide.id}
+                    >
+                        <div className="homeSlider__data">
+                            <h1 className="homeSlider__title">{this.state.currentSlide.slideTitle}</h1>
+                            <h3 className="homeSlider__desc">{this.state.currentSlide.slideSubtitle}</h3>
+                            <Link to="/sobre" className="btn">Mais info</Link>
+                        </div>
+                    </CSSTransition>
+                </TransitionGroup>
+                <div className="homeSlider__navigation">
+                    {
+                        slides.map( slide => (
+                            <button key={`selector ${slide.id}`} 
+                                    onClick={this.buttonChange} 
+                                    data-slide={slide.id}>
+                                    {`slide-${slide.id}`}
+                            </button>
+                        ))
+                    }
                 </div>
             </div>
         );
---
+++ src/helpers/slides.js
@@ -0,0 +1,21 @@
+
+
+const slides = [
+    {
+        slideTitle: 'Te ajudamos a ingressar no mundo digital!',
+        slideSubtitle: 'Desenvolvemos produtos inovadorese modernos que trazem resultados',
+        id:0
+    },
+    {
+        slideTitle: 'batata',
+        slideSubtitle: 'sdfsg',
+        id:1
+    },
+    {
+        slideTitle: 'cenoura',
+        slideSubtitle: 'casd resultados',
+        id:2
+    }
+];
+
+export default slides;
