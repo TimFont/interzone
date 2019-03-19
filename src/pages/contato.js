9d8688992a308c751717351590d82850e3a5b680
italo fontes <italo5123@hotmail.com>
2019-03-19 10:45:31 -0300

    contact page  added

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import Layout from '../components/layout';
 import Header from '../components/layouts/header'
+import SEO from '../components/seo';
+import Contato from '../components/contact/contact';
 
 export default class Contact extends Component{
     render(){
         return(
             <Layout>
+                <SEO title="Contato" keywords={[`gatsby`, `application`, `react`]} />
                 <Header titlePage="Contato" />
-                page contato works
+                <div className="contato__container">
+                    <Contato />
+                </div>
             </Layout>
         );
     }
